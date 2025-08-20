b378ac08d966ae8d89eb52685b4970432cbcb5b4
Hashir-10 <hashirzubair5742@gmail.com>
2025-08-21 00:36:02 +0500

    feat: add an event handler to take grid size as input from user and wrap the loop inside a function

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const container = document.querySelector(".container")
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.classList.add("grid-squares")
-  container.appendChild(square);
+const container = document.querySelector(".grid-container")
+const submitBtn = document.querySelector("#submitButton");
+submitBtn.addEventListener('click', (event) => {
+  const gridSizeInput = document.querySelector("#grid-size");
+  const gridSize = +gridSizeInput.value;
+  container.innerHTML = "";
+  gridSizeInput.value = "";
+ if (gridSize >= 0 && gridSize <= 100) createGrid(gridSize);
+  event.preventDefault();
+})
+
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize**2; i++) {
+    let square = document.createElement("div");
+    square.classList.add("grid-squares");
+    square.style.setProperty('--grid-rows-columns', gridSize);
+    container.appendChild(square);
+  }
 }
