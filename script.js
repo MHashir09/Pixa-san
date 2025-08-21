f91dfe04e0ace81049c0a1dd1514bfc6ae13dd07
Hashir-10 <hashirzubair5742@gmail.com>
2025-08-22 01:40:20 +0500

    feat: add necessary scripting to use iro.js library also add event listeners to square divs to change their color dynamically upon holding mouseclick

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-const container = document.querySelector(".grid-container")
+const gridContainer = document.querySelector(".grid-container")
 const submitBtn = document.querySelector("#submitButton");
+const colorPicker = new iro.ColorPicker("#colorPicker");
+
 submitBtn.addEventListener('click', (event) => {
   const gridSizeInput = document.querySelector("#grid-size");
   const gridSize = +gridSizeInput.value;
-  container.innerHTML = "";
+  gridContainer.innerHTML = "";
   gridSizeInput.value = "";
- if (gridSize >= 0 && gridSize <= 100) createGrid(gridSize);
+  if (gridSize >= 0 && gridSize <= 100) createGrid(gridSize);
   event.preventDefault();
 })
 
 function createGrid(gridSize) {
-  for (let i = 0; i < gridSize**2; i++) {
+  let isDragging = false;
+
+  for (let i = 0; i < gridSize ** 2; i++) {
     let square = document.createElement("div");
     square.classList.add("grid-squares");
     square.style.setProperty('--grid-rows-columns', gridSize);
-    container.appendChild(square);
+    gridContainer.appendChild(square);
+
+    square.addEventListener('mousedown', (event) => {
+      if (event.type == 'mousedown') {
+        isDragging = true;
+        square.style.backgroundColor = colorPicker.color.hexString;
+      }
+    });
+
+    square.addEventListener('mouseenter', () => {
+      if (isDragging == true) {
+        square.style.backgroundColor = colorPicker.color.hexString;
+      }
+    });
+
+    document.addEventListener('mouseup', () => {
+        isDragging = false;
+    })
   }
 }
